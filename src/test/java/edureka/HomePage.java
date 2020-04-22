63c09a6d5b24c0731079d128f96af5ae193f8d80
Barath <barathkumars@yahoo.com>
2020-04-21 23:22:11 -0500

    first commit

---
+++ src/test/java/edureka/HomePage.java
@@ -0,0 +1,18 @@
+package edureka;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+  @Test
+  public void f1() {
+	  System.out.println("Test1");
+  }
+  @Test
+  public void f2() {
+	  System.out.println("Test2");
+  }
+  @Test
+  public void f3() {
+	  System.out.println("Test3");
+  }
+}
