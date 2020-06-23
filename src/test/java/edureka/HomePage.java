47ccdd3b1e2ea747feb1be47da3bf89d2690c673
Barath <barathkumars@yahoo.com>
2020-06-22 22:24:54 -0500

    Reporting

--- src/test/java/edureka/HomePage.java
+++ src/test/java/edureka/HomePage.java
@@ -1,7 +1,5 @@
 package edureka;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import framework.InitialSetup;
 import framework.PageDriver;
