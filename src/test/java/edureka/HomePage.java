09651a2326ba4208562137c8dfbac24005b2eded
Barath <barathkumars@yahoo.com>
2020-06-25 19:21:55 -0500

    extend report 1st

--- src/test/java/edureka/HomePage.java
+++ src/test/java/edureka/HomePage.java
@@ -2,9 +2,13 @@ package edureka;
 
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 public class HomePage extends Setup {
 	@Test
 	public void TopRibbon() throws Throwable {
+		
+		  test.log(LogStatus.INFO, "Inside test...");
 
 		pD.Click("//input[@id='bmwradio']",Locator.xpath);
 		pD.Selects("//select[@id='carselect']",1,Locator.xpath);
--- src/test/java/edureka/Setup.java
+++ src/test/java/edureka/Setup.java
@@ -1,12 +1,20 @@
 package edureka;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import framework.InitialSetup;
 import framework.PageDriver;
 
 public class Setup extends PageDriver {
 	
 	public static PageDriver pD;
+	public static ExtentReports report;
+	public static ExtentTest test;
 	
   @BeforeSuite
   public void beforeSuite() {
@@ -15,5 +23,15 @@ public class Setup extends PageDriver {
 	  System.out.println("Inside suite");
 	  pD = new PageDriver();
 	  pD.Driver(iS.SetWebPage());
+	  
+	  report = new ExtentReports("eReport.html");
+	  test = report.startTest("Hello Report");
+
+  }
+  
+  @AfterSuite
+  public void afterSuite() {
+	  report.endTest(test);
+	  report.flush();
   }
 }
