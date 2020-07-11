7117ea9d9cbc29357d285738fd2b285ed3904c1b
Barath <barathkumars@yahoo.com>
2020-07-11 14:30:50 -0500

    Excel

--- src/main/java/utils/ReadWriteExcel.java
+++ src/main/java/utils/ReadWriteExcel.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.io.FileInputStream;
+
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -10,6 +12,20 @@ public class ReadWriteExcel {
 	XSSFSheet workSheet;
 	XSSFCell workCell;
 	
+	void readExcel() {
 	String path = "D:\\eclipse_workspace\\SeleniumFramework\\src\\test\\resources\\TestData.xlsx";
-
+	String sheetName = "Sheet1";
+	
+	try {
+		FileInputStream excelFile = new FileInputStream(path);
+		workBook = new XSSFWorkbook(excelFile);
+		workSheet = workBook.getSheet(sheetName);
+		
+		workCell = workSheet.getRow(1).getCell(1);
+		String cellData = workCell.getStringCellValue();
+		System.out.println(cellData);
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
 }
